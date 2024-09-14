ae8779eb5d93384887c45a939c312eaea4952910
hpk369 <keshruwalaharsh@gmail.com>
2024-09-14 10:56:25 -0400

    commit

---
+++ sql_load/2_dates.sql
@@ -0,0 +1,17 @@
+SELECT
+    job_title_short AS title,
+    job_location AS location,
+    job_posted_date::DATE AS date
+FROM
+    job_postings_fact;
+
+
+SELECT
+    job_title_short AS title,
+    job_location AS location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST' AS date_time,
+    EXTRACT(MONTH FROM job_posted_date) AS date_month,
+    EXTRACT(YEAR FROM job_posted_date) AS date_year
+FROM
+    job_postings_fact
+LIMIT 5;
---
+++ sql_load/3_cases.sql
@@ -0,0 +1,17 @@
+-- January jobs
+CREATE TABLE january_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(month FROM job_posted_date) = 1;
+
+-- February jobs
+CREATE TABLE february_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(month FROM job_posted_date) = 2;
+
+-- March jobs
+CREATE TABLE march_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(month FROM job_posted_date) = 3;
